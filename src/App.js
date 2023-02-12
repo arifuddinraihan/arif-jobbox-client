0623ad2fdb352874a567749e659c222d68e4571d
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-02-12 11:30:17 +0600

    added env file along with auth codes from firebase, created firebase auth and authSlice

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 
 function App() {
+
+  console.log(process.env)
   return (
     <>
       <RouterProvider router={routes} />
