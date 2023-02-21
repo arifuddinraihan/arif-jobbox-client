1d341d53b4e0b27da44810c9e47dd1892461f80c
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-02-22 00:38:08 +0600

    added commit streak

--- src/app/features/auth/authSlice.js
+++ src/app/features/auth/authSlice.js
@@ -16,12 +16,7 @@ export const createUser = createAsyncThunk("auth/createUser", async ({ email, pa
 export const getUser = createAsyncThunk("auth/getUser", async (email) => {
     const res = await fetch(`${process.env.REACT_APP_DEV_URL}/user/${email}`);
     const data = await res.json();
-    // console.log(data);
-    // console.log(data);
-    // console.log(data);
-    // console.log(data);
-    // console.log(data);
-    // console.log(data);
+    
     if (data.status) {
         return data;
     }
