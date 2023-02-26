55431d98a99e7c3bf750282ea35b4216a90854ae
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-02-26 13:30:24 +0600

    commit added

--- src/pages/message/EmployeerMessages.js
+++ src/pages/message/EmployeerMessages.js
@@ -10,6 +10,11 @@ const EmployeerMessages = () => {
     const { id } = useParams();
     const { data, isLoading, isError } = useChatWithCandidateQuery(id)
     // console.log(data);
+    // console.log(data);
+    // console.log(data);
+    // console.log(data);
+    // console.log(data);
+    // console.log(data);
     if (isLoading) {
         return <Loading />
     }
